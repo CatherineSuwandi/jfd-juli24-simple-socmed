e5bf48438ce866b3665dcb9e18d36f7f720bf7a8
unknown <suwandicatherine@gmail.com>
2024-08-22 22:04:31 +0700

    form edit

--- controller/c_profil.js
+++ controller/c_profil.js
@@ -5,5 +5,13 @@ module.exports =
             req: req,
         }
         res.render('profil/index', dataview)
-    }
+    },
+
+    form_edit: function (req,res) {
+        let dataview = {
+            req: req,
+        }
+        res.render('profil/form-edit', dataview)
+    },
+
 }
