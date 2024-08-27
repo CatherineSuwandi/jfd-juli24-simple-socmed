7a47518eddf446619490dcda6a4d65dabda30f01
unknown <suwandicatherine@gmail.com>
2024-08-27 20:17:52 +0700

    fixing the logic

--- model/m_post.js
+++ model/m_post.js
@@ -3,20 +3,22 @@ const eksekusi      = require('../config/database').eksekusi
 const moment        = require('moment')
 moment.locale('id')
 
-module.exports = 
+module.exports =
 {
-    insert: function(req) {
-        let data = {
-            caption         : req.body.form_caption,
-            file1           : req.files.form_media1.name,
-            file2           : req.files.form_media2.name,
-            file3           : req.files.form_media3.name,
-            created_at      : moment().format("YYYY-MM-DD HH:mm:ss"),
-        }
 
-        return eksekusi(mysql.format(
+    insert: function(req, file1_name, file2_name, file3_name) {
+        let data = {
+            caption     : req.body.form_caption,
+            file1       : (file1_name) ? file1_name : null,
+            file2       : (file2_name) ? file2_name : null,
+            file3       : (file3_name) ? file3_name : null,
+            created_at  : moment().format("YYYY-MM-DD HH:mm:ss"),
+        }
+        
+        return eksekusi( mysql.format(
             `INSERT INTO post SET ?`,
             [data]
         ))
     },
+
 }
